87b77c131690699490918b8a2a3b1b4cb033ea59
xingxin66 <604602098@qq.com>
2019-10-21 23:30:29 -0700

    forktest.c

---
+++ week7/forktest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include <stdlib.h>
+
+#include <sys/types.h>
+
+ 
+
+char buf[] = {"write to stdout\n"};
+
+int main()
+
+{
+
+pid_t id;
+
+ 
+freopen("test1.txt","w",stdout);
+if(write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1)
+
+perror("can't write");
+
+//printf("printf ");
+
+printf("printf\n");
+
+id = fork();
+
+if(id < 0)
+
+perror("fork error");
+
+ 
+
+exit(0);
+
+}
