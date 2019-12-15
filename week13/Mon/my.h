6609dea2735c320f4941dde546455690eb5f55cd
xingxin66 <604602098@qq.com>
2019-12-15 16:10:01 +1400

    week13--信号机制

---
+++ week13/Mon/my.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <fcntl.h>
